21d57e193bd3f9d6b16da86ce048eef759a01e58
rintu <rintu.dutta@eclicksoftwares.in>
2026-01-15 11:20:35 +0530

    update for client

--- app/(pages)/our-clients/page.tsx
+++ app/(pages)/our-clients/page.tsx
@@ -1,69 +1,87 @@
-import Comeingsoon from '@/components/common/Comeingsoon'
-import seoData from "@/data/seo.json";
-import { Metadata } from 'next';
+"use client";
+import Banner from "@/components/our-clients/Banner";
+import IndustryWiseClients from "@/components/our-clients/IndustryWiseClients";
+import { useEffect, useState } from 'react';
 
-export async function generateMetadata(): Promise<Metadata> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/our-clients/seo`, {
-        cache: "no-store", // or 'force-cache' for static
-    });
-    if (!res.ok) {
-        return seoData;
+type PageData = {
+    id: number;
+    heading: string;
+    page_feature_image: string;
+    short_description: string;
+    page_title: string;
+    description: string;
+    pages_custom_field: string;
+    page_repeater_data: string;
+};
+
+type Banner = {
+    name: string;
+    is_compoment: string;
+    d05d_title: string;
+    d05d_description: string;
+    d05d_button_name: string;
+    d05d_button_link: string;
+    d05d_image: string;
+};
+
+type PagesCustomField = {
+    banner?: Banner;
+    slug: string[];
+};
+
+const OurClientsPage = () => {
+    const [pageData, setPageData] = useState<PageData | null>(null);
+    const [hasLoading, setLoading] = useState(true);
+    const [bannerData, setBannerData] = useState<Banner | null>(null);
+    const [pageCustomField, setPageCustomField] = useState<PagesCustomField | null>(null);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/our-clients`);
+            const { response_data } = await response.json();
+            console.log('response :>> ', response);
+            setPageData(response_data);
+        } catch (err: unknown) {
+            console.error("Failed to fetch Our Clients Page:", (err as Error).message);
+        } finally {
+            setLoading(false);
+        };
     }
 
-    const {response_data:seo} = await res.json();
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-    const description = seo.meta_descriptions
-    ?.replace(/<[^>]*>?/gm, "")
-    .trim();
+    useEffect(() => {
+        if (pageData) {
+            if (pageData?.pages_custom_field) {
+                try {
+                    const customResponse = JSON.parse(pageData?.pages_custom_field ?? "{}");
+                    const data = customResponse?.group_name;
+                    setBannerData(data.banner);
+                    setPageCustomField(data)
 
-    const ogImageUrl = `${process.env.NEXT_PUBLIC_MEDIA_URL}${seo.og_image_path}`;
-    const robots = (seo.meta_robots || "").toLowerCase();
-
-    return {
-        title: seo.meta_title || seoData.title,
-        description: description || seoData.description,
-        keywords: seo.meta_keywords.split(',') || [],
-        robots: {
-            index: !robots.includes("noindex"),
-            follow: !robots.includes("nofollow"),
-        },
-        openGraph: {
-            type: "website",
-            locale: seoData.openGraph.locale,
-            siteName: seoData.openGraph.siteName,
-            url: seoData.openGraph.url,
-            title: seo.meta_title,
-            description: description,
-            images: [
-                {
-                    url: ogImageUrl,
-                    width: seo.og_image_width || 1200,
-                    height: seo.og_image_height || 630,
-                    alt: seoData.openGraph.siteName
+                } catch (err: unknown) {
+                    console.error("Error parsing custom field data:", (err as Error).message);
                 }
-            ]
-        },
-        twitter: {
-            card: "summary_large_image",
-            title: seo.meta_title,
-            description: description,
-            images: [
-                {
-                    url: ogImageUrl,
-                    width: seo.og_image_width || 1200,
-                    height: seo.og_image_height || 630,
-                    alt: seoData.openGraph.siteName
-                }
-            ]
-        },
-        alternates: {
-            canonical: seoData.openGraph.url,
-        },
-    };
+            }
+        }
+    }, [pageData]);
+    console.log('bannerData :>> ', bannerData);
+    return (
+        <div className="our_clients_page">
+            {/* ================= CLIENTS-BANNER-STARTS ================= */} 
+            {bannerData && (
+                <Banner hasLoading={hasLoading} data={bannerData} />
+            )}
+            {/* ================= CLIENTS-BANNER-ENDS ================= */}
+
+            {/* ================= INDUSTRY-WISE-CLIENTS-STARTS ================= */}
+            <IndustryWiseClients />
+            {/* ================= INDUSTRY-WISE-CLIENTS-ENDS ================= */}
+        </div>
+    );
 }
 
-const Partnerships = () => {
-  return <Comeingsoon />
-}
-
-export default Partnerships
+export default OurClientsPage
